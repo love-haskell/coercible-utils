9999442bd4803081c30d4657cf404a66d467b961
chessai <chessai1996@gmail.com>
2018-06-21 19:04:56 -0400

    add docs

--- src/CoercibleUtils.hs
+++ src/CoercibleUtils.hs
@@ -1,8 +1,58 @@
-module CoercibleUtils where
+{- |
+Utility functions for Coercible types.
+Primarily pulled from the package `newtype-generics`, and
+based on Conor McBride's Epigram work, but
+generalised to work over anything `Coercible`.
 
-import Data.Coerce
+>>> ala Sum foldMap [1,2,3,4]
+10
+
+>>> ala Endo foldMap [(+1), (+2), (subtract 1), (*2)] 3
+8
+
+>>> under2 Min (<>) 2 1
+1
+
+>>> over All not (All False)
+All {getAll = True)
+
+Users might also find the GHC plugin <https://github.com/mpickering/hashtag-coerce hashtag-coerce> useful in tandem with this library.
+-}
+module CoercibleUtils
+  ( (#.), (.#)
+  , op
+  , ala, ala'
+  , under, over
+  , under2, over2
+  , underF, overF
+  ) where
+
+import Data.Coerce (Coercible, coerce)
 
 -- | Coercive left-composition.
+--
+--   The coercive composition operators solve a problem as presented in <https://ghc.haskell.org/trac/ghc/ticket/7542 GHC Trac #7542>.
+--
+--   The problem, in a nutshell:
+--
+--   If N is a newtype constructor, then N x will always have the same
+--   representation as x (something similar applies for a newtype deconstructor).
+--   However, if f is a function,
+--   
+--   N . f = \x -> N (f x)
+--   
+--   This looks almost the same as f, but the eta expansion lifts it--the lhs could
+--   be `_|_`, but the rhs never is. This can lead to very inefficient code. Thus we
+--   steal a technique from Shachaf and Edward Kmett and adapt it to the current
+--   (rather clean) setting. Instead of using N . f, we use N #. f, which is
+--   just
+--   
+--   coerce f \`asTypeOf\` (N . f)
+--   
+--   That is, we just pretend that f has the right type, and thanks to the safety
+--   of coerce, the type checker guarantees that nothing really goes wrong. We still
+--   have to be a bit careful, though: remember that #. completely ignores the
+--   value of its left operand."
 infixr 9 #.
 (#.) :: Coercible b c => (b -> c) -> (a -> b) -> a -> c
 (#.) _ = coerce
@@ -14,6 +64,12 @@ infixr 9 .#
 (.#) f _ = coerce f
 {-# INLINE (.#) #-}
 
+-- | The first parameter is /completely ignored/ on the value level,
+--   meaning the only reason you pass in the constructor is to provide type
+--   information.
+--
+-- >>> op Identity (Identity 3)
+-- 3
 op :: Coercible a b
    => (a -> b)
    -> b
@@ -21,6 +77,22 @@ op :: Coercible a b
 op = coerce
 {-# INLINE op #-}
 
+-- | The workhorse of the package. Given a "packer" and a \"higher order function\" (/hof/),
+--   it handles the packing and unpacking, and just sends you back a regular old
+--   function, with the type varying based on the /hof/ you passed.
+--
+--   The reason for the signature of the /hof/ is due to 'ala' not caring about structure.
+--   To illustrate why this is important, consider this alternative implementation of 'under2':
+--
+--   > under2' :: (Coercible a b, Coercible a' b')
+--   >        => (a -> b) -> (b -> b -> b') -> (a -> a -> a')
+--   > under2' pa f o1 o2 = ala pa (\p -> uncurry f . bimap p p) (o1, o2)
+--
+--   Being handed the "packer", the /hof/ may apply it in any structure of its choosing –
+--   in this case a tuple.
+--
+-- >>> ala Sum foldMap [1,2,3,4]
+-- 10
 ala :: (Coercible a b, Coercible a' b')
     => (a -> b)
     -> ((a -> b) -> c -> b')
@@ -29,6 +101,19 @@ ala :: (Coercible a b, Coercible a' b')
 ala pa hof = ala' pa hof id
 {-# INLINE ala #-}
 
+-- | The way it differs from the 'ala' function in this package,
+--   is that it provides an extra hook into the \"packer\" passed to the hof.
+-- 
+--   However, this normally ends up being @id@, so 'ala' wraps this function and
+--   passes @id@ as the final parameter by default.
+--   If you want the convenience of being able to hook right into the hof,
+--   you may use this function.
+--
+-- >>> ala' Sum foldMap length ["hello", "world"]
+-- 10
+--
+-- >>> ala' First foldMap (readMaybe @Int) ["x", "42", "1"]
+-- Just 42
 ala' :: (Coercible a b, Coercible a' b')
      => (a -> b)
      -> ((d -> b) -> c -> b')
@@ -38,6 +123,10 @@ ala' :: (Coercible a b, Coercible a' b')
 ala' _ hof f = coerce #. hof (coerce f)
 {-# INLINE ala' #-}
 
+-- | A very simple operation involving running the function \'under\' the \"packer\".
+--
+-- >>> under Product (stimes 3) 3
+-- 27
 under :: (Coercible a b, Coercible a' b')
       => (a -> b)
       -> (b -> b')
@@ -46,6 +135,12 @@ under :: (Coercible a b, Coercible a' b')
 under _ f = coerce f
 {-# INLINE under #-}
 
+-- | The opposite of 'under'. I.e., take a function which works on the
+--   underlying \"unpacked\" types, and switch it to a function that works
+--   on the \"packer\".
+--
+-- >>> over All not (All False)
+-- All {getAll = True}
 over :: (Coercible a b, Coercible a' b')
      => (a -> b)
      -> (a -> a')
@@ -54,6 +149,10 @@ over :: (Coercible a b, Coercible a' b')
 over _ f = coerce f
 {-# INLINE over #-}
 
+-- | Lower a binary function to operate on the underlying values.
+--
+-- >>> under2 Any (<>) True False
+-- True
 under2 :: (Coercible a b, Coercible a' b')
        => (a -> b)
        -> (b -> b -> b')
@@ -63,6 +162,7 @@ under2 :: (Coercible a b, Coercible a' b')
 under2 _ f = coerce f
 {-# INLINE under2 #-}
 
+-- | The opposite of 'under2'.
 over2 :: (Coercible a b, Coercible a' b')
       => (a -> b)
       -> (a -> a -> a')
@@ -72,6 +172,7 @@ over2 :: (Coercible a b, Coercible a' b')
 over2 _ f = coerce f
 {-# INLINE over2 #-}
 
+-- | 'under' lifted into a 'Data.Functor.Functor'
 underF :: (Coercible a b, Coercible a' b', Functor f, Functor g)
        => (a -> b)
        -> (f b -> g b')
@@ -80,6 +181,7 @@ underF :: (Coercible a b, Coercible a' b', Functor f, Functor g)
 underF _ f = fmap coerce . f . fmap coerce
 {-# INLINE underF #-}
 
+-- | 'over' lifted into a 'Data.Functor.Functor'
 overF :: (Coercible a b, Coercible a' b', Functor f, Functor g)
       => (a -> b)
       -> (f a -> g a')
@@ -87,4 +189,3 @@ overF :: (Coercible a b, Coercible a' b', Functor f, Functor g)
       -> g b'
 overF _ f = fmap coerce . f . fmap coerce
 {-# INLINE overF #-}
-
