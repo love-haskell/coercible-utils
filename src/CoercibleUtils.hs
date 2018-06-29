2dc030ff5f2538b1e265cf0e9f7cd7ed760c2419
Simon Jakobi <simon.jakobi@gmail.com>
2018-06-29 13:18:25 +0200

    #./.#: Add examples and bottom semantics

--- src/CoercibleUtils.hs
+++ src/CoercibleUtils.hs
@@ -31,12 +31,32 @@ module CoercibleUtils
 import Data.Coerce (Coercible, coerce)
 
 -- | Coercive left-composition.
+--
+-- >>> (All #. not) True
+-- All {getAll = False}
+--
+-- The semantics with respect to bottoms are:
+--
+-- @
+-- p '#.' ⊥ ≡ ⊥
+-- p '#.' f ≡ p '.' f
+-- @
 infixr 9 #.
 (#.) :: Coercible b c => (b -> c) -> (a -> b) -> a -> c
 (#.) _ = coerce
 {-# INLINE (#.) #-}
 
 -- | Coercive right-composition.
+--
+-- >>> (stimes 2 .# Product) 3
+-- Product {getProduct = 9}
+--
+-- The semantics with respect to bottoms are:
+--
+-- @
+-- ⊥ '.#' p ≡ ⊥
+-- f '.#' p ≡ p '.' f
+-- @
 infixr 9 .#
 (.#) :: Coercible a b => (b -> c) -> (a -> b) -> a -> c
 (.#) f _ = coerce f
